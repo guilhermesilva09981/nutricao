f3466a6593c83757de3eabb7fe1c64d5b293a905
guigui_bolsonaro <silva.taborda@escola.pr.gov.br>
2025-10-07 09:16:09 -0300

    45

--- js/principal.js
+++ js/principal.js
@@ -1,9 +1,20 @@
-var titulo = document.querySelector("h1");
+var titulo = document.querySelector("h1")
 console.log(titulo);
 console.log(titulo.textContent);
-titulo.textContent("Aparecida Nutricionista");
+titulo.textContent = ("Nickin Nutricionista");
+
+
 var paciente = document.querySelector("#primeiro-paciente");
-console.log(paciente)
-var tdPeso = paciente.querySelector(".info-peso");
-console.log(paciente);// tr
-console.log(tdPeso);//td que tem o peso
+var tdPeso = document.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+
+console.log(paciente);
+console.log(peso);
+
+
+var tdAltura = document.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+console.log(altura);
+
+
