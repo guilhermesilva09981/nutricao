45c83732b1f3dd63f49ff1d5a581e1089a0ea02c
guigui_bolsonaro <silva.taborda@escola.pr.gov.br>
2025-10-10 10:01:37 -0300

    dfg

--- js/principal.js
+++ js/principal.js
@@ -1,20 +1,48 @@
-var titulo = document.querySelector("h1")
+//console.log(document.querySelector("h1"));
+var titulo = document.querySelector("h1");
 console.log(titulo);
 console.log(titulo.textContent);
-titulo.textContent = ("Nickin Nutricionista");
+titulo.textContent = ("Guilherme nutritations");
+
+
 
 
 var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = document.querySelector(".info-peso");
+var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
-
-
 console.log(paciente);
-console.log(peso);
+console.log(tdPeso);
 
 
-var tdAltura = document.querySelector(".info-altura");
+
+
+var tdAltura = document.querySelector("#primeiro-paciente");
+var altura = document.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 console.log(altura);
 
+
+
+
+//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+var pesoehValido = true;
+ var alturaehValido = true;
+
+
+if (peso < 0 || peso > 1000) {
+  console.log("peso inválido");
+  var pesoehValido = false;
+}
+
+
+if(altura < 0||altura > 3.00){
+   console.log("Altura inválida");
+    var alturaehValido = false;
+}
+
+
+ if(alturaEhValida && pessoEhValido){
+   var imc = peso / (altura * altura) :
+   tdImc.textContentc = imc;
+ }
 
