e5a70911faf48c62e23903bbb3b2165d568f02e1
guigui_bolsonaro <silva.taborda@escola.pr.gov.br>
2025-10-24 10:53:58 -0300

    3

--- js/principal.js
+++ js/principal.js
@@ -1,10 +1,17 @@
 var titulo = document.querySelector("h1");
 console.log(titulo);
 console.log(titulo.textContent);
-titulo.textContent = ("Pedrinho nutrição");
+titulo.textContent = ("Aparecida Nutricionista");
+
+
+
+
+21/10
 
 
 var pacientes = document.querySelectorAll(".paciente");
+
+
 for (var i = 0; i < pacientes.length; i++) {
 
 
@@ -27,22 +34,43 @@ for (var i = 0; i < pacientes.length; i++) {
 
 
    if (peso <= 0 || peso >= 1000) {
-       console.log("Peso invalido");
+       console.log("Peso inválido!");
        pesoEhValido = false;
-       tdImc.textContent = "Peso ivalido";
+       tdImc.textContent = "Peso inválido";
+       // paciente.style.backgroundColor = "red";
+       paciente.classList.add("paciente-invalido");
    }
 
 
    if (altura <= 0 || altura >= 3.00) {
-       console.log("Altura invalida!");
+       console.log("Altura inválida!");
        alturaEhValida = false;
-       tdImc.textContent = imc.toFixed(2)
+       tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
    }
 
 
-   if (alturaEhValida && pesoEhValido) {
+ if (alturaEhValida && pesoEhValido) {
        var imc = peso / (altura * altura);
-       tdImc.textContent = imc;
+       tdImc.textContent = imc.toFixed(2);
    }
 }
+
+
+// 23/10
+
+
+// titulo.addEventListener() adiciona um escutador de eventos.
+// PRIMEIRO MOSTRAR ESSA - DEPOIS ALTERA-LA PARA FICAR ANONIMA
+titulo.addEventListener("click", mostraMensagem);
+function mostraMensagem(){
+   alert("Olá eu fui clicado.")
+}
+
+
+// funcao anonima = a de cima mas diferente
+titulo.addEventListener("click", function(){
+   alert("Ola eu fui clicado numa funcao anonima");
+});
+
 
